ca8b4099c8425b179f7b1c1301495e9e8ab29690
nemuineru <suteado16x16@gmail.com>
2025-08-10 23:44:56 +0900

    waterstream edit. also font rework

--- Assets/Nemuke Industry/1week_Hiku/Script/Environment/WaterStream.cs
+++ Assets/Nemuke Industry/1week_Hiku/Script/Environment/WaterStream.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class WaterStream : MonoBehaviour
 {
+    [SerializeField]
+    float Power = 3.0f;
+    [SerializeField]
+    float effectRange = 3.0f;
     // Start is called before the first frame update
-    void Start()
+    void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "Player" || other.tag == "Item")
+        {
+            var Rigid = other.GetComponent<Rigidbody>();
+            Vector3 vectTowards = Vector3.Project(other.transform.position - transform.position, transform.forward);
+            Rigid.AddForce(vectTowards.normalized * Power * (effectRange - vectTowards.magnitude));
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
